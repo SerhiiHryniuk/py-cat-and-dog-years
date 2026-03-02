ccf43ee574cfe8a66b91f056428b32e06c9f7a17
Serhii Hryniuk <serhiibhryniuk@gmail.com>
2026-03-02 10:14:58 +0200

    Solution: written tests and then function

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,34 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_age",
+    [
+        (-1, -1, [0, 0]),
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (23, 23, [1, 1]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ], ids=[
+        "should return zeros when negative ages",
+        "should return zeros when ages is zero",
+        "should return zeros when ages less than 15",
+        "should return ones when ages greater-equal than 15 and less than 24",
+        "should return proper year in different species rules",
+        "should return proper human age when cat and dogs ages is very high"
+    ]
+)
+def test_should_calculate_proper_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected_age: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_age
+
+
+def test_should_raise_type_error() -> None:
+    with pytest.raises(TypeError):
+        get_human_age(2, "3")
